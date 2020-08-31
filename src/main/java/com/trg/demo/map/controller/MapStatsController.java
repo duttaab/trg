0cd6f306b08dd4678cf1a43a0d5ad615006843dc
duttaab <duttaa@hotmail.com>
2020-08-31 17:08:26 +0530

    Removed logs

--- src/main/java/com/trg/demo/map/controller/MapStatsController.java
+++ src/main/java/com/trg/demo/map/controller/MapStatsController.java
@@ -12,24 +12,12 @@
 
 package com.trg.demo.map.controller;
 
-import java.util.List;
 import java.util.TreeMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.trg.demo.map.apierror.MapMessageException;
-import com.trg.demo.map.apierror.RestApiExceptionHandler;
-import com.trg.demo.map.model.MapModelAlertServiceCentre;
 import com.trg.demo.map.model.MapStatsModel;
-import com.trg.demo.map.service.ProcessMapAlertServiceCenter;
-import com.trg.demo.map.service.ProcessMapMessage;
 import com.trg.demo.map.service.ProcessReadyForSM;
 
 @RequestMapping("map/stats/v3")
@@ -45,9 +33,6 @@ public class MapStatsController {
 	
 	@GetMapping
 	public TreeMap<String, MapStatsModel>  getStats() {
-		
-		System.out.println("GET Method Called");
 	    return this.service.getStats();
-
 	}
 }
